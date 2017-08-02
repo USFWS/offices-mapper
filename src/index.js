5371cc4a2ae307f3d7961875df9271ed07331ccf
Roy Hewitt <roy_hewitt@fws.gov>
2017-08-02 12:48:32 -0400

    Wrap our App in react-router, render to dom

--- src/index.js
+++ src/index.js
@@ -1,8 +1,32 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { render } from 'react-dom';
+import { Router, IndexRoute, Route, browserHistory } from 'react-router';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import configureStore from './store/configureStore';
+import { loadState, saveState } from './store/localStorage';
+
+import App from './components/App';
+import ListView from './components/list/ListView';
+import AboutView from './components/about/AboutView';
+import MapView from './components/map/MapView';
+
+import './index.css';
+
+const persistedState = loadState();
+const store = configureStore(persistedState);
+
+store.subscribe(() => {
+  saveState(store.getState());
+});
+
+render(
+  <Provider store={store}>
+    <Router history={browserHistory}>
+      <Route path="/" component={App} >
+        <IndexRoute component={ListView} />
+        <Route path="/about" component={AboutView} />
+        <Route path="/map" component={MapView} />
+      </Route>
+    </Router>
+  </Provider>, document.getElementById('root'));
