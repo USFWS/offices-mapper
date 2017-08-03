46be1dc3bdbb8623f52e14db103abb756a230c68
Roy Hewitt <roy_hewitt@fws.gov>
2017-08-03 09:15:33 -0400

    Add zoom out icon to how to menu

--- src/components/sidebar/HowTo.js
+++ src/components/sidebar/HowTo.js
@@ -4,10 +4,11 @@ import Geolocate from '../icons/Geolocate';
 import Goose from '../icons/Goose';
 import Hatchery from '../icons/Hatchery';
 import Building from '../icons/Building';
+import ZoomOut from '../icons/ZoomOut';
 
 const HowTo = props => {
   return (
-    <div className="content-detail">
+    <div className="content-detail how-to">
       <h2>Southeast Region Office Map</h2>
 
       <p>Use the map to find a U.S. Fish and Wildlife Service field office, National Wildlife Refuge, or a National Fish Hatchery near you. For more information about a specific office click an office marker on the map.</p>
@@ -16,6 +17,7 @@ const HowTo = props => {
 
       <ul>
         <li><Geolocate /> Find the nearest offices</li>
+        <li><ZoomOut /> Zoom to full extent</li>
       </ul>
 
       <h3>Office Types</h3>
