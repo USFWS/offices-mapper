bbaa64b88700e9f25cfa537b415fda1205d0c103
Roy Hewitt <roy_hewitt@fws.gov>
2017-08-03 09:16:05 -0400

    Add zoom out button to map, which clears the query causing us to render all offices and zoom to their bounds

--- src/components/sidecar/Sidecar.js
+++ src/components/sidecar/Sidecar.js
@@ -2,8 +2,10 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { findNearest } from '../../api/OfficesAPI';
 import * as officeActions from '../../actions/offices';
+import * as queryActions from '../../actions/query';
 
 import Geolocate from '../icons/Geolocate';
+import ZoomOut from '../icons/ZoomOut';
 
 class Sidecar extends React.Component {
   constructor(props) {
@@ -30,6 +32,12 @@ class Sidecar extends React.Component {
             <Geolocate loading={ this.state.loading ? 'loading' : ''} />
           </button>
         </li>
+        <li>
+          <button className='sidecar-link' onClick={this.props.zoomToFullExtent}>
+            <span className='sidecar-label'>Zoom to Full Extent</span>
+            <ZoomOut />
+          </button>
+        </li>
       </ul>
     );
   }
@@ -43,7 +51,8 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
   return {
-    selectNearestOffices: offices => dispatch(officeActions.nearestOffices(offices))
+    selectNearestOffices: offices => dispatch(officeActions.nearestOffices(offices)),
+    zoomToFullExtent: () => dispatch(queryActions.update(''))
   }
 }
 
