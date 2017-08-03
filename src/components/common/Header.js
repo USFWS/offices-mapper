1922998150613e599db8cd966bd8ee490c8133f9
Roy Hewitt <roy_hewitt@fws.gov>
2017-08-03 09:15:14 -0400

    Add USFWS logo component to header

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Link, IndexLink } from 'react-router';
 
+import Logo from '../icons/Logo';
+
 class Header extends React.Component {
   render() {
     return (
       <header>
         <nav>
           <ul className="main-nav">
-            <li><a href='https://www.fws.gov/'><span>FWS.gov</span></a></li>
+            <li><a href='https://www.fws.gov/'><Logo /></a></li>
             <li><IndexLink to="/" activeClassName="active"><span>List</span></IndexLink></li>
             <li><Link to="/map" activeClassName="active"><span>Map</span></Link></li>
             <li><Link to="/about" activeClassName="active"><span>About</span></Link></li>
