b55b052ff6fd08e577bfdefa2503b27ea35fb117
Roy Hewitt <roy_hewitt@fws.gov>
2017-08-23 07:43:48 -0400

    Prettier format

--- src/components/sidebar/CurrentOffice.js
+++ src/components/sidebar/CurrentOffice.js
@@ -3,22 +3,26 @@ import React from 'react';
 import Links from './Links';
 
 const CurrentOffice = ({ office }) => {
-
-  const buildImageUrl = name => `https://usfws.github.io/southeast-mega-map/images/${name}`;
+  const buildImageUrl = name =>
+    `https://usfws.github.io/southeast-mega-map/images/${name}`;
   return (
     <div className="content-detail">
-      { office.image ? <img src={buildImageUrl(office.image)} alt={office.alt}/> : null }
-      <h2>{office.name}</h2>
+      {office.image
+        ? <img src={buildImageUrl(office.image)} alt={office.alt} />
+        : null}
+      <h2>
+        {office.name}
+      </h2>
       <Links office={office} />
-      <p dangerouslySetInnerHTML={{ __html: office.narrative }}></p>
+      <p dangerouslySetInnerHTML={{ __html: office.narrative }} />
 
       <h3>Address</h3>
-        <address>
-          {office.address} <br />
-          {office.city}, {office.state}
-        </address>
+      <address>
+        {office.address} <br />
+        {office.city}, {office.state}
+      </address>
     </div>
-  )
-}
+  );
+};
 
 export default CurrentOffice;
